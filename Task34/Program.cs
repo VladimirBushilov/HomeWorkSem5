c1251e2e68961e16b78f59af2fc4d22c7d129d30
VladimirBushilov <ttkshibo@ya.ru>
2023-05-27 16:25:51 +0300

    форматирование

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -4,31 +4,37 @@
 // [345, 897, 568, 234] -> 2*/
 
 
-Console.WriteLine("Введите размер массива ");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-FillArrayRandomNumbers(numbers);
-PrintArray(numbers);
-int Count = 0;
-
-for (int a = 0; a < numbers.Length; a++)
-    if (numbers[a] % 2 == 0)
-        Count++;
-
-Console.WriteLine($"всего {numbers.Length} чисел, {Count} из них чётные");
-
-void FillArrayRandomNumbers(int[] numbers)
+internal class Program
 {
-    for (int i = 0; i < numbers.Length; i++)
+    private static void Main(string[] args)
     {
-        numbers[i] = new Random().Next(100, 1000);
+        Console.WriteLine("Введите размер массива ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[size];
+        FillArrayRandomNumbers(numbers);
+        PrintArray(numbers);
+        int Count = 0;
+
+        for (int a = 0; a < numbers.Length; a++)
+            if (numbers[a] % 2 == 0)
+                Count++;
+
+        Console.WriteLine($"всего {numbers.Length} чисел, {Count} из них чётные");
+
+        void FillArrayRandomNumbers(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = new Random().Next(100, 1000);
+            }
+        }
+        void PrintArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+        }
     }
-}
-void PrintArray(int[] numbers)
-{
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        Console.Write(numbers[i] + " ");
-    }
-    Console.WriteLine();
 }
--- Task36/Program.cs
+++ Task36/Program.cs
@@ -5,17 +5,23 @@
 // [-4, -6, 89, 6] -> 0
 
 
-int[] numbers = new int[5];
-Random rand = new Random();
-for (int i = 0; i < numbers.Length; i++)
+internal partial class Program
 {
-    numbers[i] = rand.Next(-100, 101);
-    Console.Write($"{numbers[i]}, ");
+    private static void Main(string[] args)
+    {
+        int[] numbers = new int[5];
+        Random rand = new Random();
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = rand.Next(-100, 101);
+            Console.Write($"{numbers[i]}, ");
+        }
+        int sum = 0;
+        for (int i = 1; i < numbers.Length; i += 2)
+        {
+            sum += numbers[i];
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {sum}");
+    }
 }
-int sum = 0;
-for (int i = 1; i < numbers.Length; i += 2)
-{
-    sum += numbers[i];
-}
-Console.WriteLine();
-Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {sum}");
